c15185f6e3115a801fcecfc0c01bf714f28faf77
Elysia <23010777@st.phenikaa-uni.edu.vn>
2026-08-24 09:05:11 +0700

    post

--- My-Node-Project/index.js
+++ My-Node-Project/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-    res.send('お帰りなさい、ご飯にする、おふろにする、それとも…わ　た　し？');
+    res.send('お帰りなさい、ご飯にする、おふろにする、それとも…わ...た...し？');
 });
 
 app.get('/aboutHikari', (req, res) => {
@@ -13,7 +16,17 @@ app.get('/aboutHikari', (req, res) => {
     });
 });
 
+app.post('/aboutHikari', (req, res) => {
+    const { id, name } = req.body;
+    res.json({
+        id,
+        name
+    });
+});
 
+app.post('/', (req, res) => {
+    res.json('お帰りなさい、ご飯にする、おふろにする、それとも…わ...た...し？');
+});
 
 const port = 3000;
 app.listen(port, () => {
